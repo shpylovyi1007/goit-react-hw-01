ceb9eb7552cd31fd3c942709f31b4ad160eef3b7
shpylovyi1007 <yevheniishpylovyi398@gmail.com>
2024-07-01 11:38:27 +0300

    1 part

---
+++ src/components/App/App.jsx
@@ -0,0 +1,31 @@
+import Profile from "../Profile/Profile";
+
+const userData = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+const App = () => {
+  const { username, tag, location, avatar, stats } = userData;
+
+  return (
+    <>
+      <Profile
+        name={username}
+        tag={tag}
+        location={location}
+        image={avatar}
+        stats={stats}
+      />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div>
+      <div>
+        <img src={avatar} alt="User avatar" />
+        <p>{username}</p>
+        <p>{tag} </p>
+        <p>{location} </p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers: </span>
+          <span>{stats.followers} </span>
+        </li>
+        <li>
+          <span>Views: </span>
+          <span>{stats.views} </span>
+        </li>
+        <li>
+          <span>Likes: </span>
+          <span>{stats.likes} </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
